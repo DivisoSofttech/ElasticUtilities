5857f34b3d6abbd6c1495886ff8f602686354195
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-11-07 11:49:12 +0530

    add kafka

---
+++ src/main/java/com/diviso/graeshoppe/config/MessageBinderConfiguration.java
@@ -0,0 +1,18 @@
+package com.diviso.graeshoppe.config;
+
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.cloud.stream.annotation.Input;
+
+
+//import com.diviso.graeshoppe.customer.avro.Customer;
+
+public interface MessageBinderConfiguration {
+
+	String CUSTOMER="customer";
+	
+	//String ORDER ="order";
+	
+	/*@Input(CUSTOMER)
+	KStream<String, Customer> customer();*/
+	
+}
