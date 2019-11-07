cf5298259784c9112dec75ab3dfda5102845f96f
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-11-07 12:30:27 +0530

    clean Store

--- src/main/java/com/diviso/graeshoppe/config/MessageBinderConfiguration.java
+++ src/main/java/com/diviso/graeshoppe/config/MessageBinderConfiguration.java
@@ -3,6 +3,8 @@ package com.diviso.graeshoppe.config;
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.cloud.stream.annotation.Input;
 
+import com.diviso.graeshoppe.avro.Customer;
+
 
 //import com.diviso.graeshoppe.customer.avro.Customer;
 
@@ -12,7 +14,7 @@ public interface MessageBinderConfiguration {
 	
 	//String ORDER ="order";
 	
-	/*@Input(CUSTOMER)
-	KStream<String, Customer> customer();*/
+	@Input(CUSTOMER)
+	KStream<String, Customer> customer();
 	
 }
--- src/main/java/com/diviso/graeshoppe/web/rest/StreamConsumerService.java
+++ src/main/java/com/diviso/graeshoppe/web/rest/StreamConsumerService.java
@@ -28,77 +28,14 @@ public class StreamConsumerService {
 	@Autowired
 	private CustomerService customerService;
 
-	/*@SuppressWarnings("static-access")
 	@StreamListener(MessageBinderConfiguration.CUSTOMER)
 	public void listenToPayment(KStream<String, Customer> message) {
-		message.foreach((key, value) -> {
-			System.out.println("payment Value consumed is " + value);
-			Optional<OrderMasterDTO> orderMaster = orderMasterService.findByOrderNumber(value.getTargetId());
-
-			if (orderMaster.isPresent()) {
-				OrderMasterDTO orderMasterDTO = orderMaster.get();
-				if (!value.getPaymentType().equals("cod")) {
-					LOG.info("Order paid");
-					orderMasterDTO.setOrderStatus("ORDER PAID");
-				} else {
-					LOG.info("Order Not paid");
-					orderMasterDTO.setOrderStatus("ORDER NOT PAID");
-				}
-				orderMasterService.save(orderMasterDTO);
-			} else {
-
-				CompletableFuture<String> completableFuture = new CompletableFuture<>();
-				completableFuture.runAsync(() -> {
-					System.out.println("Inside RunAsync+++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-					boolean executeFlag = true;
-					int count = 0;
-					while (executeFlag) {
-						System.out.println("Inside Loop " + count);
-						try {
-							Thread.sleep(2000l);
-							System.out.println("Thread is went to sleep" + Thread.currentThread().getName());
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-						Optional<OrderMasterDTO> dto = orderMasterService.findByOrderNumber(value.getTargetId());
-						if (dto.isPresent()) {
-							System.out.println("Inside If check ++++++++++++++++++++++++++++++++++");
-							OrderMasterDTO orderMasterDTO = dto.get();
-							if (!value.getPaymentType().equals("cod")) {
-								LOG.info("Order paid");
-								orderMasterDTO.setOrderStatus("ORDER PAID");
-							} else {
-								LOG.info("Order Not paid");
-								orderMasterDTO.setOrderStatus("ORDER NOT PAID");
-							}
-							orderMasterService.save(orderMasterDTO);
-							executeFlag = false;
-							completableFuture.complete("Completed");
-						}
-						count++;
-					}
-
-				});
-*/
-				/*
-				 * try { Thread.sleep(10000l); OrderMasterDTO orderMasterDTO =
-				 * orderMaster.get(); if (!value.getPaymentType().equals("cod")) {
-				 * LOG.info("Order paid"); orderMasterDTO.setOrderStatus("ORDER PAID"); } else {
-				 * LOG.info("Order Not paid"); orderMasterDTO.setOrderStatus("ORDER NOT PAID");
-				 * } orderMasterService.save(orderMasterDTO); } catch (InterruptedException e) {
-				 * e.printStackTrace(); }
-				 */
-		/*	}
-
+		message.foreach((key,value)->{
+			System.out.println("consumed values is "+value);
 		});
-	}*/
-/*
-	@StreamListener(MessageBinderConfiguration.ORDER)
-	public void listenToOrder(KStream<String, Order> message) {
-		message.foreach((key, value) -> {
-			System.out.println("order Value consumed is " + value);
-			orderMasterService.convertAndSaveOrderMaster(value);
-		});
-	}*/
+		
+	}
+
+	
 
 }
