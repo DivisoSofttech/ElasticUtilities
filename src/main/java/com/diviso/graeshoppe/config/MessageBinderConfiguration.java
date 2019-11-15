d29f0476216b584da4ee9742f371831f900d840c
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-11-15 16:49:38 +0530

    add store-group

--- src/main/java/com/diviso/graeshoppe/config/MessageBinderConfiguration.java
+++ src/main/java/com/diviso/graeshoppe/config/MessageBinderConfiguration.java
@@ -3,9 +3,9 @@ package com.diviso.graeshoppe.config;
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.cloud.stream.annotation.Input;
 
-import com.diviso.graeshoppe.avro.CustomerInfo;
+//import com.diviso.graeshoppe.avro.CustomerInfo;
 import com.diviso.graeshoppe.store.avro.Store;
-import com.diviso.graeshoppe.avro.ContactInfo;
+//import com.diviso.graeshoppe.avro.ContactInfo;
 
 public interface MessageBinderConfiguration {
 
@@ -15,12 +15,12 @@ public interface MessageBinderConfiguration {
 	
 	String STORE ="store";
 	
-	@Input(CUSTOMER)
+	/*@Input(CUSTOMER)
 	KStream<String, CustomerInfo> customer();
 	
 	@Input(CONTACT)
 	KStream<String, ContactInfo> contact();
-	
+	*/
 	@Input(STORE)
 	KStream<String,Store> store();
 }
